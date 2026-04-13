024a157c77bc295040a6e017f7d6446787a29536
23300240034-chenyuxuan <23300240034@m.fudan.edu.cn>
2026-04-13 22:06:49 +0800

    feat: 完成了IR到QUAD的基本转换逻辑，实现了17个visitor书写

---
+++ HW5/build/lib/ir/CMakeFiles/ir.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ir.dir/canon.cc.o"
+  "CMakeFiles/ir.dir/canon.cc.o.d"
+  "CMakeFiles/ir.dir/treep.cc.o"
+  "CMakeFiles/ir.dir/treep.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ir.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW5/build/lib/util/CMakeFiles/util.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/fducompilerh2026/fducompilerh2026/HW5/lib/util/print_config.cc" "lib/util/CMakeFiles/util.dir/print_config.cc.o" "gcc" "lib/util/CMakeFiles/util.dir/print_config.cc.o.d"
+  "/root/fducompilerh2026/fducompilerh2026/HW5/lib/util/quad2xml.cc" "lib/util/CMakeFiles/util.dir/quad2xml.cc.o" "gcc" "lib/util/CMakeFiles/util.dir/quad2xml.cc.o.d"
+  "/root/fducompilerh2026/fducompilerh2026/HW5/lib/util/tree2xml.cc" "lib/util/CMakeFiles/util.dir/tree2xml.cc.o" "gcc" "lib/util/CMakeFiles/util.dir/tree2xml.cc.o.d"
+  "/root/fducompilerh2026/fducompilerh2026/HW5/lib/util/xml2tree.cc" "lib/util/CMakeFiles/util.dir/xml2tree.cc.o" "gcc" "lib/util/CMakeFiles/util.dir/xml2tree.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
