0aa6d209c85e8c662d5f12502c958d73f6734e59
23300240034-chenyuxuan <23300240034@m.fudan.edu.cn>
2026-05-07 14:28:37 +0800

    feat: 完成了HW8的全部内容并且得到正确输出

---
+++ HW8/build/lib/opt/CMakeFiles/opt.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opt.dir/opt.cc.o"
+  "CMakeFiles/opt.dir/opt.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ HW8/build/lib/util/CMakeFiles/util.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/util.dir/print_config.cc.o"
+  "CMakeFiles/util.dir/print_config.cc.o.d"
+  "CMakeFiles/util.dir/xml2quad.cc.o"
+  "CMakeFiles/util.dir/xml2quad.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/util.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
